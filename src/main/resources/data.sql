05f21b3ae9766ca0e9a2c92f08ffd477a0d07d13
yushi0620honda <japan20jun1996@gmail.com>
2022-10-02 23:33:53 +0900

    todoListの作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,10 @@ INSERT INTO todo_details (id, title, is_done, time_limit)
 VALUES(1, 'ジャガイモ', 'true', '2022-10-01');
 
 INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(2, 'ニンジン', 'false', '2022-10-01');
+VALUES(2, 'ニンジン', 'true', '2022-10-02');
 
 INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(3, 'タマネギ', 'false', '2022-10-01');
+VALUES(3, 'タマネギ', 'true', '2022-10-03');
 
 INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(4, 'カレー粉', 'false', '2022-10-01');
+VALUES(4, 'カレー粉', 'false', '2022-10-04');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS todo_details (
 	id INTEGER PRIMARY KEY,
-	title VARCHAR(40) UNIQUE KEY,
+	title VARCHAR(40),
 	is_done BOOLEAN,
 	time_limit DATE
 );
