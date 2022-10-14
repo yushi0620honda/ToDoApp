7c1c83576ced070653da8b7e29105630705eae3e
yushi0620honda <91672424+yushi0620honda@users.noreply.github.com>
2022-10-15 07:35:38 +0900

    tableの修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,7 @@
 --todo_details
 INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(1, 'ジャガイモ', 'true', '2022-10-01');
-
-INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(2, 'ニンジン', 'false', '2022-10-01');
-
-INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(3, 'タマネギ', 'false', '2022-10-01');
-
-INSERT INTO todo_details (id, title, is_done, time_limit)
-VALUES(4, 'カレー粉', 'false', '2022-10-01');
+VALUES
+	(1, 'ジャガイモを買う', 'true', '2022-10-01'),
+	(2, 'ニンジンを買う', 'false', '2022-10-01'),
+	(3, 'タマネギを買う', 'false', '2022-10-01'),
+	(4, 'カレー粉を買う', 'false', '2022-10-01');
