cf2a19d434092b0368437314207e226f5c391260
yushi0620honda <91672424+yushi0620honda@users.noreply.github.com>
2022-10-27 21:35:32 +0900

    Revert "todo add formの作成"

--- src/main/java/com/example/demo/todoApp/domain/repository/UserDao.java
+++ src/main/java/com/example/demo/todoApp/domain/repository/UserDao.java
@@ -18,10 +18,6 @@ public interface UserDao {
 
 	public List<User> selectTodoList(int id) throws DataAccessException;
 
-	public void insertTodoDetailTrue(UserForm userForm) throws DataAccessException;
-
-	public void insertTodoDetailFalse(UserForm userForm) throws DataAccessException;
-
 	public boolean count(String title) throws DataAccessException;
 
 	public void deleteTodoList() throws DataAccessException;
--- src/main/java/com/example/demo/todoApp/domain/service/UserService.java
+++ src/main/java/com/example/demo/todoApp/domain/service/UserService.java
@@ -39,15 +39,6 @@ public class UserService {
 		return dao.selectTodoList(id);
 	}
 
-	public void insertTodoDetailTrue(UserForm userForm) {
-		dao.insertTodoDetailTrue(userForm);
-	}
-
-	public void insertTodoDetailFalse(UserForm userForm) {
-		dao.insertTodoDetailFalse(userForm);
-	}
-
-
 	public boolean count(String title) {
 		return dao.count(title);
 	}
